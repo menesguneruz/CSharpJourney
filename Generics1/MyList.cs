791f586cffdf9531a824e931e39012d043dc132a
Mustafa Enes Güneruz <mustafaguneruz19@gmail.com>
2021-01-31 19:41:07 +0300

    Studied on Generics

---
+++ Generics1/MyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics1
+{
+    // We are making  a kind of List class manually.
+    class MyList<Type>
+    {
+        Type[] items;
+
+        //Constructor Method. This method's name is same with the class
+        // Constructor method runs automatically when we create a new object from the class.
+        public MyList()
+        {
+            items = new Type[0];
+        }
+
+        public void Add(Type item)
+        {
+            Type[] tempArray = items;
+            items = new Type[items.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
---
+++ Generics1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Generics1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Ali");
+        }
+    }
+}
