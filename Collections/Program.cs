5245b3f8c1d37b7533b977e5c5b1c9c6582aed3e
Mustafa Enes Güneruz <mustafaguneruz19@gmail.com>
2021-01-31 18:16:59 +0300

    Studied on Lists

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -7,7 +8,16 @@ namespace Collections
         static void Main(string[] args)
         {
             // Arrays's disadvantage is you cannot move out of the array's area. For example you can't add fourth element to an array which has three elements ideally.
-            // C# solves this problem by using collections.
+            // C# solves this problem by using collections. 
+
+            List<string> names1 = new List<string> {"Mustafa", "Enes", "Sadık", "Taylan"};
+            Console.WriteLine(names1[0]);
+            Console.WriteLine(names1[1]);
+            Console.WriteLine(names1[2]);
+            Console.WriteLine(names1[3]);
+            names1.Add("Arda");
+            Console.WriteLine(names1[4]);
+            Console.WriteLine(names1[0]);
         }
     }
 }
