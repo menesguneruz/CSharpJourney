f5f85fc5a3770e234bd32def8e1e1087aa47b3d6
Mustafa Enes Güneruz <mustafaguneruz19@gmail.com>
2021-01-31 16:32:27 +0300

    Add project files.

---
+++ CSharpJourney/BasketManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class BasketManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Added to the basket");
+        }
+    }
+}
---
+++ CSharpJourney/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Name = "Apple";
+            product1.Price = 15;
+            product1.Description = "Apple from Amasya Turkey";
+
+            Product product2 = new Product();
+
+            product2.Name = "Watermelon";
+            product2.Price = 40;
+            product2.Description = "Delicious Watermelons form Diyarbakir";
+
+            Product[] products = new Product[] {product1, product2};
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine("--------------------");
+            }
+
+            Console.WriteLine("----------Methods---------");
+
+            BasketManager basketManager = new BasketManager();
+            basketManager.Add();
+        }
+    }
+}
