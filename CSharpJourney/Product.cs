e9a5d7133c4e5007f5704ce111713864556dbe68
Mustafa Enes Güneruz <mustafaguneruz19@gmail.com>
2021-01-31 17:13:17 +0300

    Studied on Object Oriented Programming (Classes) Fundamentals

--- CSharpJourney/Product.cs
+++ CSharpJourney/Product.cs
@@ -6,7 +6,7 @@ namespace Methods
 {
     public class Product
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string Name { get; set; } 
         public double Price { get; set; }
         public string Description { get; set; }
---
+++ ObjectOriented1/ProductManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOriented1
+{
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            //There have to be operation codes in real life.
+            Console.WriteLine(product.ProductName + " Added!");
+        }
+    }
+}
